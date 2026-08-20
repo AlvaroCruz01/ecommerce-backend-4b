9d01d666abd9d91d74815ee6c04a2b5ab37ec5e0
AlvaroCruz01 <273732@unifio.edu.br>
2026-08-20 01:15:58 +0000

    Projeto Inicial

---
+++ ecommerce/src/main/java/br/edu/unifio/ecommerce/entidades/Categoria.java
@@ -0,0 +1,26 @@
+package br.edu.unifio.ecommerce.entidades;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+// @NoArgsConstructor
+// @AllArgsConstructor
+// @Data
+
+
+// byte short int long Byte Shart Integer Long
+// floot double Floot Double
+// char Character
+// boolean Boolean
+public class Categoria {
+    private Short id;
+    private String nome;
+    private String descricao;
+
+
+}
